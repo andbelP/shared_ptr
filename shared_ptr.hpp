ecb12aaa6c16852e97ef97cf2fa7cfaa4b29e84d
Andrey <andbel2@bk.ru>
2026-06-30 22:04:22 +0300

    feat: implemented some methods

--- shared_ptr.hpp
+++ shared_ptr.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-#include "control_block.hpp"
 #include <utility>
 
-template<typename T>
+#include "control_block.hpp"
+
+template <typename T>
 class weak_ptr;
 
 template <typename T>
@@ -11,73 +12,145 @@ class shared_ptr {
     T* data_{};
     ControlBlock* cblock_{};
 
-
-    template<typename U>
+    template <typename U>
     friend class weak_ptr;
 
-    template<typename U>
+    template <typename U>
     friend class shared_ptr;
 
-    template<typename U>
+    template <typename U>
     shared_ptr(const weak_ptr<U>& ptr);
 
    public:
-
-    void swap(shared_ptr& other){
-        std::swap(data_, other.data_);
-        std::swap(cblock_, other.cblock_);
-    }
+    void swap(shared_ptr& other) noexcept;
 
     shared_ptr() = default;
 
-    template<typename U>
-    shared_ptr(U* data){
-        if(!data){
-            return;
-        }
+    template <typename U>
+    shared_ptr(U* data);
 
-        data_=data;
-        cblock_=new TypedControlBlock<T>{1,0,data_};
-    }
+    shared_ptr(std::nullptr_t);
 
-    shared_ptr(std::nullptr_t){}
+    shared_ptr(const shared_ptr& ptr);
 
-    shared_ptr(const shared_ptr& ptr){
-        data_=ptr.data_;
-        cblock_=ptr.cblock_;
+    template <typename U>
+    shared_ptr(const shared_ptr<U>& ptr);
 
-        if(cblock_){
-            cblock_->IncreaseStrong();
-        }
-    }
+    shared_ptr& operator=(const shared_ptr& ptr);
 
-    template<typename U>
-    shared_ptr(const shared_ptr<U>& ptr){
-        data_=ptr.data_;
-        cblock_=ptr.cblock_;
+    T& operator*() const noexcept;
 
-        if(cblock_){
-            cblock_->IncreaseStrong();
-        }
-    }
+    T* operator->() const noexcept;
 
-    shared_ptr& operator=(const shared_ptr& ptr){
-        shared_ptr tmp = ptr;
-        swap(tmp);
-        return *this;
-    }
+    T* get() const noexcept;
+
+    template <typename U>
+    shared_ptr& operator=(const shared_ptr<U>& ptr);
+
+    long use_count() const noexcept;
+
+    void reset() noexcept;
 
     template<typename U>
-    shared_ptr& operator=(const shared_ptr<U>& ptr){
-        shared_ptr tmp = ptr;
-        swap(tmp);
-        return *this;
-    }
+    void reset(U* ptr);
 
-    ~shared_ptr(){
-        if(cblock_){
-            cblock_->DecreaseStrong();
-        }
-    }
-
+    ~shared_ptr();
 };
+
+template <typename T>
+void shared_ptr<T>::swap(shared_ptr& other) noexcept {
+    std::swap(data_, other.data_);
+    std::swap(cblock_, other.cblock_);
+}
+
+template <typename T>
+template <typename U>
+shared_ptr<T>::shared_ptr(U* data) {
+    if (!data) {
+        return;
+    }
+
+    data_ = data;
+    cblock_ = new TypedControlBlock<T>{1, 0, data_};
+}
+
+template <typename T>
+shared_ptr<T>::shared_ptr(std::nullptr_t) {}
+
+template <typename T>
+shared_ptr<T>::shared_ptr(const shared_ptr<T>& ptr) {
+    data_ = ptr.data_;
+    cblock_ = ptr.cblock_;
+
+    if (cblock_) {
+        cblock_->IncreaseStrong();
+    }
+}
+
+template <typename T>
+template <typename U>
+shared_ptr<T>::shared_ptr(const shared_ptr<U>& ptr) {
+    data_ = ptr.data_;
+    cblock_ = ptr.cblock_;
+
+    if (cblock_) {
+        cblock_->IncreaseStrong();
+    }
+}
+
+template <typename T>
+shared_ptr<T>& shared_ptr<T>::operator=(const shared_ptr<T>& ptr) {
+    shared_ptr tmp = ptr;
+    swap(tmp);
+    return *this;
+}
+
+template <typename T>
+template <typename U>
+shared_ptr<T>& shared_ptr<T>::operator=(const shared_ptr<U>& ptr) {
+    shared_ptr tmp = ptr;
+    swap(tmp);
+    return *this;
+}
+
+template <typename T>
+shared_ptr<T>::~shared_ptr() {
+    if (cblock_) {
+        cblock_->DecreaseStrong();
+    }
+}
+
+template<typename T>
+long shared_ptr<T>::use_count() const noexcept{
+    if(cblock_){
+        return cblock_->strong_cnt;
+    }
+    return 0;
+}
+
+template<typename T>
+void shared_ptr<T>::reset() noexcept{
+    shared_ptr<T>{}.swap(*this);
+}
+
+template<typename T>
+template<typename U>
+void shared_ptr<T>::reset(U* ptr){
+    shared_ptr<T> tmp(ptr);
+    swap(tmp);
+}
+
+template<typename T>
+T& shared_ptr<T>::operator*() const noexcept{
+    return *data_;
+}
+
+template<typename T>
+T* shared_ptr<T>::operator->() const noexcept{
+    return data_;
+}
+
+template<typename T>
+T* shared_ptr<T>::get() const noexcept{
+    return data_;
+}
--- weak_ptr.hpp
+++ weak_ptr.hpp
@@ -2,6 +2,7 @@
 
 #include <exception>
 #include <string>
+#include <utility>
 
 #include "control_block.hpp"
 #include "shared_ptr.hpp"
@@ -10,13 +11,14 @@ class bad_weak_ptr : public std::exception {
     std::string msg_;
 
    public:
+    bad_weak_ptr(std::string_view data);
 
-    bad_weak_ptr(std::string_view data) : msg_(data){}
-
-    const char* what() const noexcept override {
-        return msg_.data();
-    }
+    const char* what() const noexcept override;
 };
+
+inline bad_weak_ptr::bad_weak_ptr(std::string_view data) : msg_(data) {}
+
+inline const char* bad_weak_ptr::what() const noexcept { return msg_.data(); }
 
 template <typename T>
 class weak_ptr {
@@ -32,26 +34,73 @@ class weak_ptr {
 
    public:
     template <typename U>
-    weak_ptr(const shared_ptr<U>& ptr) {
-        data_ = ptr.data_;
-        cblock_ = ptr.cblock_;
+    weak_ptr(const shared_ptr<U>& ptr);
 
-        if (cblock_) {
-            cblock_->IncreaseWeak();
-        }
-    }
+    weak_ptr(const shared_ptr<T>& ptr);
 
-    weak_ptr(const shared_ptr& ptr) {
-        data_ = ptr.data_;
-        cblock_ = ptr.cblock_;
+    weak_ptr() = default;
 
-        if (cblock_) {
-            cblock_->IncreaseWeak();
-        }
-    }
+    template <typename U>
+    weak_ptr(const weak_ptr<U>& ptr);
 
-    bool expired() { return !cblock_ || cblock_->strong_cnt > 0; }
+    weak_ptr(const weak_ptr& ptr);
+
+    weak_ptr& operator=(const weak_ptr& other);
+
+    template <typename U>
+    weak_ptr& operator=(const weak_ptr<U>& other);
+
+    void swap(weak_ptr& other) noexcept;
+
+    void reset() noexcept;
+
+    shared_ptr<T> lock();
+
+    bool expired() const;
+
+    long use_count() const noexcept;
+
 };
+
+template <typename T>
+void weak_ptr<T>::swap(weak_ptr& other) noexcept {
+    std::swap(data_, other.data_);
+    std::swap(cblock_, other.cblock_);
+}
+
+template <typename T>
+template <typename U>
+weak_ptr<T>::weak_ptr(const shared_ptr<U>& ptr) {
+    data_ = ptr.data_;
+    cblock_ = ptr.cblock_;
+
+    if (cblock_) {
+        cblock_->IncreaseWeak();
+    }
+}
+
+template <typename T>
+weak_ptr<T>::weak_ptr(const shared_ptr<T>& ptr) {
+    data_ = ptr.data_;
+    cblock_ = ptr.cblock_;
+
+    if (cblock_) {
+        cblock_->IncreaseWeak();
+    }
+}
+
+template <typename T>
+shared_ptr<T> weak_ptr<T>::lock() {
+    if (expired()) {
+        return shared_ptr<T>{};
+    }
+    return shared_ptr<T>(*this);
+}
+
+template <typename T>
+bool weak_ptr<T>::expired() const {
+    return !cblock_ || cblock_->strong_cnt > 0;
+}
 
 template <typename T>
 template <typename U>
@@ -59,7 +108,56 @@ shared_ptr<T>::shared_ptr(const weak_ptr<U>& other) {
     if (other.expired()) {
         throw bad_weak_ptr("Can't initialize shared_ptr with expired weak_ptr");
     }
-    data_=other.data_;
-    cblock_=other.cblock_;
+    data_ = other.data_;
+    cblock_ = other.cblock_;
     cblock_->IncreaseStrong();
 };
+
+template <typename T>
+weak_ptr<T>& weak_ptr<T>::operator=(const weak_ptr<T>& other) {
+    weak_ptr tmp = other;
+    swap(tmp);
+    return *this;
+}
+
+template <typename T>
+template <typename U>
+weak_ptr<T>& weak_ptr<T>::operator=(const weak_ptr<U>& other) {
+    weak_ptr tmp = other;
+    swap(tmp);
+    return *this;
+}
+
+template<typename T>
+void weak_ptr<T>::reset() noexcept{
+    weak_ptr<T>{}.swap(*this);
+}
+
+template<typename T>
+long weak_ptr<T>::use_count() const noexcept{
+    if(!cblock_){
+        return 0;
+    }
+    return cblock_->strong_cnt;
+}
+
+template <typename T>
+weak_ptr<T>::weak_ptr(const weak_ptr<T>& ptr) {
+    data_ = ptr.data_;
+    cblock_ = ptr.cblock_;
+
+    if (cblock_) {
+        cblock_->IncreaseWeak();
+    }
+}
+
+template <typename T>
+template <typename U>
+weak_ptr<T>::weak_ptr(const weak_ptr<U>& ptr) {
+    data_ = ptr.data_;
+    cblock_ = ptr.cblock_;
+
+    if (cblock_) {
+        cblock_->IncreaseWeak();
+    }
+}
